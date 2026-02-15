723741f14dd9dd4a2d4580d5d8436c45dca7dc25
Aaron Creighton <aaron.s.creighton@gmail.com>
2026-02-15 14:29:54 +1300

    update name of template

---
+++ Shiny_template_full/R/db_mf_dbTableActions.R
@@ -0,0 +1,30 @@
+
+# this is a module function files example for doing actions to a Table
+# used if you need the global variables in r for the users session
+
+
+#notes on name spaces for module actions
+# could be used to differentiate a delete action or update action (change what is done)
+# could be used to provide context to inform the error handeling
+
+
+mf_getTable <- function (id, r,
+                         in_value = NULL,
+                         in_value2){
+  moduleServer(id, function(input, output, session) {
+
+
+    return(out_value)
+  })
+}
+
+mf_updateTable <- function (id, r,
+                         in_value = NULL,
+                         in_value2){
+  moduleServer(id, function(input, output, session) {
+
+
+    return(out_value)
+  })
+}
+
---
+++ Shiny_template_full/global.R
@@ -0,0 +1,31 @@
+# libraries
+
+library(shinydashboard)
+
+# hide warnings etc.
+suppressWarnings(suppressMessages(library(tidyverse)))
+
+
+#Bookmarking####
+enableBookmarking(store = "url")
+
+
+#log event to console
+console_log <- function(x){
+  message(paste0("LOG - ",Sys.time()," - ",x))
+}
+
+#pull in functions for development
+#source("R/moduleFunctions.R", local = TRUE)
+#ource("R/generalFunctions.R", local = TRUE)
+
+# declare global static variables here e.g.:
+
+## user identification based on windows login
+user <- Sys.info()["user"] %>%
+  as.character() %>%
+  str_to_title()
+
+## global static lists etc
+
+CustomerList <- c("A","B", "C")
